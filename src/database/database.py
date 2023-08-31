2971b3a2e40d6ece56122e7e5058c74d0f394e04
Dan Bliss <danbliss14027@gmail.com>
2023-08-31 19:05:35 -0400

    Attempted at using postgres

--- src/database/database.py
+++ src/database/database.py
@@ -1,14 +1,14 @@
 import os
 
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_PROD_DATABASE_URL = "sqlite:///./db/prod_hub.db"
-SQLALCHEMY_DEV_DATABASE_URL = "sqlite:///./db/dev_hub.db"
+SQLALCHEMY_DEV_DATABASE_URL = "postgresql://admin:password123@db/hub"
 
 engine = create_engine(
-    SQLALCHEMY_PROD_DATABASE_URL if os.getenv("ENVIRONMENT") == "production" else SQLALCHEMY_DEV_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_PROD_DATABASE_URL if os.getenv("ENVIRONMENT") == "production" else SQLALCHEMY_DEV_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
