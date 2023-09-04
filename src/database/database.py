042d391d869addd9e2c1a6f2f366e186e8d186ba
Dan Bliss <danbliss14027@gmail.com>
2023-09-04 15:32:16 -0400

    more tweaks

--- src/database/database.py
+++ src/database/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_PROD_DATABASE_URL = "sqlite:///./db/prod_hub.db"
-SQLALCHEMY_DEV_DATABASE_URL = "postgresql://admin:password123@db/hub"
+SQLALCHEMY_DEV_DATABASE_URL = "postgresql://postgres:example@db/mun"
 
 engine = create_engine(
     SQLALCHEMY_PROD_DATABASE_URL if os.getenv("ENVIRONMENT") == "production" else SQLALCHEMY_DEV_DATABASE_URL
